c79ff3b01077e646fb43c873f86eff1763d7b3e8
Paweł Krzaczkowski <pawel@freshmind.pl>
2018-07-25 11:12:00 +0200

    tests

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -26,6 +26,7 @@ class ClientTest extends TestCase
         $client->setClient($http_client);
 
         $client->printers->labels([
+            'rotate' => 180,
             'zpl' => '^xa^cfa,50^fo100,100^fdHello World^fs^xz',
         ]);
         foreach ($container as $transaction) {
@@ -35,4 +36,32 @@ class ClientTest extends TestCase
             $this->assertTrue($accept == "image/png");
         }
     }
+
+    /**
+     * @expectedException \GuzzleHttp\Exception\GuzzleException
+     */
+    public function testBasicClientMissingZpl()
+    {
+        $mock = new MockHandler([
+            new Response(200, ['Content-Type' => 'image/png'], "1234"),
+        ]);
+        $container = [];
+        $history = Middleware::history($container);
+        $stack = HandlerStack::create($mock);
+        $stack->push($history);
+        $http_client = new Client(['handler' => $stack]);
+        $client = new LabelaryClient();
+        $client->setClient($http_client);
+
+        $client->printers->labels([
+            'rotate' => 180,
+        ]);
+        foreach ($container as $transaction) {
+            /** @var \GuzzleHttp\Psr7\Request $request */
+            $request = $transaction['request'];
+            $accept = $request->getHeaders()['Accept'][0];
+            $this->assertTrue($accept == "image/png");
+        }
+    }
+
 }
